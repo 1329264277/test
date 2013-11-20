93fc3e50d1f386571eb4a67643cb3e0db403265c
think <think@geisha-THINK>
2013-11-20 15:13:38 +0800

    first commit

---
+++ src/com/example/s_1/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.s_1;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+	 
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		this.setContentView(R.layout.activity_2);
+		super.onResume();
+	}
+	@Override
+	protected void onStart() {
+		// TODO Auto-generated method stub
+		this.setContentView(R.layout.activity_2);
+		super.onStart();
+	}
+}
